19d26ac66469fe02b154c2b3688945fbcf4acf4d
huntoor <omarahmed99929@gmail.com>
2023-07-23 12:45:01 +0300

    first commit

---
+++ src/components/homePage/HomeSection.js
@@ -0,0 +1,17 @@
+import homeImage from "../../assets/images/home.jpg";
+import "./home.css";
+
+function HomeSection() {
+
+  return (
+    <section id="home">
+      <div className="wrapper">
+        <nav className="nav">
+          
+        </nav>
+      </div>
+    </section>
+  );
+}
+
+export default HomeSection;
