c17eb8425989ee59748a1e32369000c0eb04c16a
huntoor <omarahmed99929@gmail.com>
2023-07-30 20:10:55 +0300

    about page done | Readme.md file changed

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Krunch Clone
 
 #### Original website url: [Krunch](https://krunch.webflow.io/#about)
+
+## Getting Started 
+### How to start
+To install the dependencies to the local **node_modules** folder run:
+```
+npm install
+```
+
+To run the app in the development mode use:
+```
+npm start
+```
+Open [http://localhost:3000](http://localhost:3000) to view the application in your browser.
+
--- src/components/aboutPage/AboutSection.js
+++ src/components/aboutPage/AboutSection.js
@@ -4,8 +4,19 @@ function AboutSection() {
   return (
     <>
       <Navbar />
-      <div className="wrapper" style={{textAlign: "center"}}>
-        <h1>Under construcation GO AWAAAY!!! ヽ༼ ಠ益ಠ ༽ﾉ</h1>
+      <div className="wrapper" style={{ textAlign: "center" }}>
+        {/* <h1>Under construcation GO AWAAAY!!! ヽ༼ ಠ益ಠ ༽ﾉ</h1> */}
+        <div className="main-header">
+          <h2>About us</h2>
+          <p>Setibulum rutrum quam vitae fringilla tincidunt</p>
+          <div className="line-border"></div>
+        </div>
+
+        <div className="the-iframe">
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe className="embed-responsive-item" width="848" height="476" src="https://www.youtube.com/embed/lLWEXRAnQd0" title="Bob Ross - Island in the Wilderness (Season 29 Episode 1)" frameBorder="" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+          </div>
+        </div>
       </div>
     </>
   )
