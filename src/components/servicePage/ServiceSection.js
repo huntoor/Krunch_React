7a50d39bddf2e98ff52ea85d1d968b325d7531d5
huntoor <omarahmed99929@gmail.com>
2023-08-01 21:22:31 +0300

    services Section done

--- src/components/servicePage/ServiceSection.js
+++ src/components/servicePage/ServiceSection.js
@@ -1,12 +1,76 @@
-import Navbar from "../Navbar/Navbar"
+import "./service.css";
+
+import Navbar from "../Navbar/Navbar";
+
+import {
+  FaDesktop,
+  FaCode,
+  FaAndroid,
+  FaCamera,
+  FaWandMagicSparkles,
+  FaEarthAfrica
+}
+  from
+  "react-icons/fa6";
 
 function ServicesSection() {
   return (
     <>
       <Navbar />
-      <div className="wrapper" style={{textAlign: "center"}}>
-        <h1>Under construcation GO AWAAAY!!! ヽ༼ ಠ益ಠ ༽ﾉ</h1>
+      <div className="wrapper" style={{ textAlign: "center" }}>
+        {/* <h1>Under construcation GO AWAAAY!!! ヽ༼ ಠ益ಠ ༽ﾉ</h1> */}
+        <div className="main-header">
+          <h2>Services</h2>
+          <p>Scope of our featured premium services</p>
+          <div className="line-border"></div>
+        </div>
+
+        <div className="grid">
+          <div className="block">
+            <FaDesktop className="icon" />
+            <h5>Web Design</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique.
+              Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae.</p>
+          </div>
+
+          <div className="block">
+            <FaCode className="icon" />
+            <h5>Web Design</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique.
+              Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae.</p>
+          </div>
+
+          <div className="block">
+            <FaAndroid className="icon" />
+            <h5>App Design</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique.
+              Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae.</p>
+          </div>
+
+          <div className="block">
+            <FaCamera className="icon" />
+            <h5>photography</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique.
+              Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae.</p>
+          </div>
+
+          <div className="block">
+            <FaWandMagicSparkles className="icon" />
+            <h5>Branding</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique.
+              Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae.</p>
+          </div>
+
+          <div className="block">
+            <FaEarthAfrica className="icon" />
+            <h5>Hosting</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim eros elementum tristique.
+              Duis cursus, viverra ornare, eros dolor interdum nulla, commodo diam libero vitae.</p>
+          </div>
+        </div>
       </div>
+
+
     </>
   )
 }
